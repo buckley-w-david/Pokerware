0d8cf2f88c9dc64efffaff824cacc3fee0220e9c
David Buckley <buckley.w.david@gmail.com>
2019-07-10 22:32:07 -0400

    update package description

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-build: $(shell find pokerware -type f)
+build: setup.py $(shell find pokerware -type f)
 	python3 setup.py sdist bdist_wheel
 
 release: build
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
       name='pokerware',
       version='0.1.2',
-      description="A small example package",
+      description="Generate passwords via playing cards",
       long_description_content_type="text/markdown",
       long_description=long_description,
       classifiers=[
