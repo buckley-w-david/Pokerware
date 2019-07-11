6242f2c585c87ac560d8ce1a9a432197bab8807b
David Buckley <buckley.w.david@gmail.com>
2019-07-10 22:28:05 -0400

    bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
       name='pokerware',
-      version='0.1.1',
+      version='0.1.2',
       description="A small example package",
       long_description_content_type="text/markdown",
       long_description=long_description,
